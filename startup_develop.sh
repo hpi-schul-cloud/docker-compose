a628031731df8d8c2167f987372bf9096062f60b
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-11-03 17:04:13 +0100

    seperate calendar and notification service

--- startup_develop.sh
+++ startup_develop.sh
@@ -1,5 +1,7 @@
 docker-compose \
 	-f ./compose-files/docker-compose.yml \
 	-f ./compose-files/docker-compose.ldap.yml \
+	-f ./compose-files/docker-compose.calendar.yml \
+	-f ./compose-files/docker-compose.notification.yml \
 	-f ./compose-files/docker-compose.dev.yml \
 	$@
