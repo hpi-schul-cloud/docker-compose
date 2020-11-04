956d6ade475dbe3e5e368acf196837b85b4cc9ee
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-11-04 12:47:55 +0100

    add configurable paths

--- startup_develop.sh
+++ startup_develop.sh
@@ -1,7 +1,11 @@
+source develop.env
 docker-compose \
+	--env-file ./env/develop.env \
 	-f ./compose-files/docker-compose.yml \
 	-f ./compose-files/docker-compose.ldap.yml \
 	-f ./compose-files/docker-compose.calendar.yml \
 	-f ./compose-files/docker-compose.notification.yml \
+	-f ./compose-files/docker-compose.file.yml \
+	-f ./compose-files/docker-compose.superhero.yml \
 	-f ./compose-files/docker-compose.dev.yml \
 	$@
