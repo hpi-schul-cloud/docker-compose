e7c925b31631bb775e260e5106f140ff8bfbfa50
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-11-05 15:47:58 +0100

    change env file

--- startup_mocha-client-tests.sh
+++ startup_mocha-client-tests.sh
@@ -3,6 +3,6 @@ source ./envs/end-to-end-tests.env
 source ./envs/version.env
 
 docker-compose \
-	--env-file ./env/develop.env \
+	--env-file ./env/end-to-end-tests.env \
 	-f ./compose-files/docker-compose.yml \
 	${@:-up -d client}
