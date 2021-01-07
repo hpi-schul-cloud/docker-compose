9b871983d20339210a8d80e54432561f1acae92c
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-01-07 13:15:03 +0100

    re-use existing env names for docker login

--- scripts/dockerhub.login.sh
+++ scripts/dockerhub.login.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# helper to authenticate against dockerhub using credentials given in DOCKER_USERNAME (username) and DOCKER_PASSWORD (token)
+# helper to authenticate against dockerhub using credentials given in DOCKER_ID (username) and MY_DOCKER_PASSWORD (token)
 
-# authenticate docker when DOCKER_PASSWORD is given
-if [[ -z "$DOCKER_USERNAME" || -z "$DOCKER_PASSWORD" ]]
+# authenticate docker when docker credentials given
+if [[ -z "$DOCKER_ID" || -z "$MY_DOCKER_PASSWORD" ]]
 then
     echo "docker authentication incomplete or missing: do not authenticate against docker hub..."
 else 
     echo "docker authentication given: authenticate against docker hub..."
-    echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+    echo $MY_DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 fi
