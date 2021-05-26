b36693a90960f9f02267175da61f2cf2400c40b4
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2021-05-26 13:43:21 +0200

    log into docker before pull

--- startup_develop.sh
+++ startup_develop.sh
@@ -4,6 +4,8 @@ source ./envs/default.env
 source ./envs/develop.env
 [ -f ./envs/.env ] && source ./envs/.env
 
+echo "$MY_DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+
 [ -n "${FILE_PREVIEW_PATH}" ] || docker pull schulcloud/filepreview:${FILE_PREVIEW_DOCKER_TAG:-latest}
 [ -n "${NOTIFICATION_SERVICE_PATH}" ] || docker pull schulcloud/node-notification-service:${NOTIFICATION_SERVICE_DOCKER_TAG}
 [ -n "${CALENDAR_PATH}" ] || docker pull schulcloud/schulcloud-calendar:${CALENDAR_DOCKER_TAG:-latest}
