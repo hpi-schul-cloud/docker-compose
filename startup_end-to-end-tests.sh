f9d04e098a92bd785011c2506995a8694a238173
Tobias W <tobias.wollowski@hpi.de>
2021-07-22 17:20:34 +0200

    remove notification service

--- startup_end-to-end-tests.sh
+++ startup_end-to-end-tests.sh
@@ -8,7 +8,6 @@ source ./envs/end-to-end-tests.env
 docker-compose \
 	--env-file ./envs/end-to-end-tests.env \
 	-f ${COMPOSE_FILES_PATH}/docker-compose.yml \
-	-f ${COMPOSE_FILES_PATH}/docker-compose.notification.yml \
 	-f ${COMPOSE_FILES_PATH}/docker-compose.calendar.yml \
 	-f ${COMPOSE_FILES_PATH}/docker-compose.end-to-end-tests.yml \
 	${@:-up -d}
